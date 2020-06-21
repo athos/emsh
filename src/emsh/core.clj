9d6e4531fe4d02657fbf0bef1c103267154806d7
Shogo Ohta <athos0220@gmail.com>
2020-06-21 23:01:35 +0900

    Forward stdout & stderr unless they are redirected

--- src/emsh/core.clj
+++ src/emsh/core.clj
@@ -2,39 +2,72 @@
   (:refer-clojure :exclude [< >])
   (:require [clojure.java.io :as io]
             [emsh.compile :as comp])
-  (:import [java.io OutputStream BufferedReader]))
+  (:import [java.io OutputStream BufferedReader]
+           [java.lang ProcessBuilder$Redirect]))
 
-(defn- ^Process ensure-started [p]
+(defrecord ProcessProxy [process])
+
+(defn- ^Process process-impl [process-proxy]
+  (:process process-proxy))
+
+(defn- start-process [^ProcessBuilder pb]
+  (let [out (.redirectOutput pb)
+        err (.redirectError pb)]
+    (cond-> (->ProcessProxy (.start pb))
+      (not= out ProcessBuilder$Redirect/PIPE)
+      (assoc :out out)
+
+      (not= err ProcessBuilder$Redirect/PIPE)
+      (assoc :err err))))
+
+(defn- ensure-started [p]
   (if (instance? ProcessBuilder p)
-    (.start ^ProcessBuilder p)
+    (start-process p)
     p))
+
+(defn- input-stream [p]
+  (when (nil? (:out p))
+    (.getInputStream (process-impl p))))
+
+(defn- forward-error-stream [p]
+  (when (nil? (:err p))
+    (future
+      (io/copy (.getErrorStream (process-impl p)) *err*))))
+
+(defn- with-input-stream [p f]
+  (let [p' (ensure-started p)]
+    (forward-error-stream p')
+    (when-let [in (input-stream p')]
+      (f in))))
 
 (defn ->str [x]
   (if (instance? ProcessBuilder x)
-    (slurp (.getInputStream (ensure-started x)))
+    (with-input-stream x slurp)
     (str x)))
 
 (defn ->str* [p]
-  (let [sb (StringBuilder.)]
-    (with-open [r (-> (ensure-started p)
-                      .getInputStream
-                      io/reader)]
-      (loop []
-        (when-let [line (.readLine ^BufferedReader r)]
-          (.append sb line)
-          (recur))))
-    (.toString sb)))
+  (with-input-stream p
+    (fn [in]
+      (let [sb (StringBuilder.)]
+        (with-open [r (io/reader in)]
+          (loop []
+            (when-let [line (.readLine ^BufferedReader r)]
+              (.append sb line)
+              (recur))))
+        (.toString sb)))))
 
 (defn ->lines [p]
-  (line-seq (io/reader (.getInputStream (ensure-started p)))))
+  (with-input-stream p #(line-seq (io/reader %))))
 
 (defn ->out
   ([p] (->out p *out*))
   ([p out]
-   (io/copy (.getInputStream (ensure-started p)) out)))
+   (with-input-stream p #(io/copy % out))))
 
 (defn wait-for [p]
-  (.waitFor (ensure-started p)))
+  (let [p' (ensure-started p)]
+    (->out p')
+    (.waitFor (process-impl p'))))
 
 (defn succeeded? [p]
   (= (wait-for p) 0))
